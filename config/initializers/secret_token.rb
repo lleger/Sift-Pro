8af3455a688f2cccab8eaec847f23261876162fe
Logan Leger <logan@loganleger.com>
2012-03-06 12:16:32 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siftpro::Application.config.secret_token = '2d120c7566f220df530e6b420f81797431a464206910e778117b8c36c697663598824bb1abd23a275b771e766a407517f9c3d47c6f9d58661052a43f44811972'
