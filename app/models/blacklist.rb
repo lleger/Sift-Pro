d179c1bac9815798e63390a5164f70e59e892b43
Logan Leger <logan@loganleger.com>
2012-03-08 13:06:11 -0600

    Importing big default blacklist, earfucker

--- app/models/blacklist.rb
+++ app/models/blacklist.rb
@@ -4,20 +4,8 @@ class Blacklist < ActiveRecord::Base
   
   validates :word, presence: true
   
-  def self.discriminatory
-    ["bitch", "bastard", "beaner", "nigger"]
-  end
-  
-  def self.profane
-    ["fuck", "damn", "bastard", "shit"]
-  end
-  
-  def self.sexual
-    ["cunt", "ax wound", "blow job", "cock", "pussy"]
-  end
-  
   def self.default
-    discriminatory + profane + sexual
+    DEFAULT_BLACKLIST
   end
   
   def self.all_with_default
