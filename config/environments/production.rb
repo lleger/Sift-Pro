b2d80a08d81b869daa717b04380815816582bc1a
Logan Leger <logan@loganleger.com>
2012-03-09 02:05:02 -0600

    Fix for production email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,6 @@ Siftpro::Application.configure do
   config.action_mailer.default_url_options = { :host => 'siftpro.com' }
   
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_charset = "utf-8"
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
